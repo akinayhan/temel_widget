0651ba4933f3edab187136e6903cd116df6f106e
akın ayhan <akinayhan@hotmail.com.tr>
2021-04-13 00:20:18 +0300

    dart

--- lib/main.dart
+++ lib/main.dart
@@ -4,12 +4,11 @@ void main() {
   runApp(MaterialApp(home: MyApp()));
 }
 
-
 class MyApp extends StatelessWidget {
   String mesaj = "Merhaba ilk uygulamam!";
   @override
   Widget build(BuildContext context) {
-    var ogrenciler = ["Akın Ayhan","Ayhan Akın","Akın Akın","Ayhan Ayhan"];
+    var ogrenciler = ["Akın Ayhan", "Ayhan Akın", "Akın Akın", "Ayhan Ayhan"];
     return Scaffold(
       appBar: AppBar(
         title: Text(mesaj),
@@ -17,36 +16,42 @@ class MyApp extends StatelessWidget {
       body: Column(
         children: <Widget>[
           Expanded(
-            child: ListView.builder(
-              itemCount: ogrenciler.length,
-              itemBuilder: (BuildContext context, int index){
-                return Text(ogrenciler[index]);
-              })
-          ),
+              child: ListView.builder(
+                  itemCount: ogrenciler.length,
+                  itemBuilder: (BuildContext context, int index) {
+                    return Text(ogrenciler[index]);
+                  })),
           Center(
             child: RaisedButton(
               child: Text("Sonucu gör"),
               onPressed: () {
-                int puan = 60;
-                String mesaj = "";
-                if (puan >= 50) {
-                  mesaj = "Geçti";
-                } else if (puan >= 40) {
-                  mesaj = "Bütünlemeye kaldı";
-                } else {
-                  mesaj = "Kaldı";
-                }
-
-                var alert = AlertDialog(
-                  title: Text("Sınav Sonucu"),
-                  content: Text(mesaj),
-                );
-                showDialog(context: context, builder: (BuildContext) => alert);
+                var mesaj = sinavHesapla(55);
+                mesajGoster(context,mesaj);
               },
             ),
           ),
         ],
       ),
     );
+  }
+
+  String sinavHesapla(int puan) {
+    String mesaj = "";
+    if (puan >= 50) {
+      mesaj = "Geçti";
+    } else if (puan >= 40) {
+      mesaj = "Bütünlemeye kaldı";
+    } else {
+      mesaj = "Kaldı";
+    }
+    return mesaj;
+  }
+
+  void mesajGoster(BuildContext context, String mesaj) {
+    var alert = AlertDialog(
+      title: Text("Sınav Sonucu"),
+      content: Text(mesaj),
+    );
+    showDialog(context: context, builder: (BuildContext) => alert);
   }
 }
