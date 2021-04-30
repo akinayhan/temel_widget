a84faf1ee3f12df912b7d7255b19ff97b95455aa
akın ayhan <akinayhan@hotmail.com.tr>
2021-04-30 18:19:05 +0300

    30.04

--- lib/main.dart
+++ lib/main.dart
@@ -1,41 +1,39 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:temel_widget/models/student.dart';
 
 void main() {
   runApp(MaterialApp(home: MyApp()));
 }
 
-class MyApp extends StatelessWidget {
-  String mesaj = "Merhaba ilk uygulamam!";
+class MyApp extends StatefulWidget {
+  @override
+  _MyAppState createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  String mesaj = "Öğrenci Takip Sistemi";
+
+  String seciliOgrenci = "ABC ";
+
+  List<Student> students = [Student("Akın","Ayhan",25),Student("Burak", "Dinç", 65),Student("Çağrı","Tunç",45)];
+
+  var ogrenciler = ["Akın Ayhan",
+    "Burak Dinç",
+    "Çağrı Tunç",
+    "Osman Gül"
+  ];
+
   @override
   Widget build(BuildContext context) {
-    var ogrenciler = ["Akın Ayhan", "Ayhan Akın", "Akın Akın", "Ayhan Ayhan"];
+
     return Scaffold(
       appBar: AppBar(
         title: Text(mesaj),
       ),
-      body: Column(
-        children: <Widget>[
-          Expanded(
-              child: ListView.builder(
-                  itemCount: ogrenciler.length,
-                  itemBuilder: (BuildContext context, int index) {
-                    return Text(ogrenciler[index]);
-                  })),
-          Center(
-            // ignore: deprecated_member_use
-            child: RaisedButton(
-              child: Text("Sonucu gör"),
-              onPressed: () {
-                var mesaj = sinavHesapla(55);
-                mesajGoster(context,mesaj);
-              },
-            ),
-          ),
-        ],
-      ),
+      body: buildBody(context)
     );
   }
-
 
   String sinavHesapla(int puan) {
     String mesaj = "";
@@ -55,5 +53,54 @@ class MyApp extends StatelessWidget {
       content: Text(mesaj),
     );
     showDialog(context: context, builder: (BuildContext) => alert);
+  }
+
+  Widget buildBody(BuildContext context) {
+    return Column(
+      children: <Widget>[
+        Expanded(
+            child: ListView.builder(
+                itemCount: students.length,
+                itemBuilder: (BuildContext context, int index) {
+                  return ListTile(
+                    leading: CircleAvatar(
+                      backgroundImage: NetworkImage("https://cdn.pixabay.com/photo/2018/06/27/07/45/student-3500990_960_720.jpg"),
+                    ),
+                    title: Text(students[index].firstName + " " + students[index].lastName),
+                    subtitle: Text("Sınavdan aldığı not: " + students[index].grade.toString() + " ["+students[index].getStatus+"]"),
+                    trailing: buildStatusIcon(students[index].grade),
+                    onTap: (){
+                      setState(() {
+                        seciliOgrenci = students[index].firstName + " " + students[index].lastName;
+                      });
+
+                      print(seciliOgrenci);
+                    },
+                  );
+                })),
+        Text("Seçili öğrenci : " + seciliOgrenci),
+        Center(
+          // ignore: deprecated_member_use
+          child: RaisedButton(
+            child: Text("Sonucu gör !!!"),
+            onPressed: () {
+              var mesaj = sinavHesapla(55);
+              mesajGoster(context,mesaj);
+            },
+          ),
+        ),
+      ],
+    );
+
+  }
+
+  Widget buildStatusIcon(int grade) {
+    if (grade >= 50) {
+      return Icon(Icons.done);
+    } else if (grade >= 40) {
+      return Icon(Icons.album);
+    } else {
+      return Icon(Icons.clear);
+    }
   }
 }
---
+++ lib/models/student.dart
@@ -0,0 +1,33 @@
+class Student{
+  String firstName;
+  String lastName;
+  int grade;
+  String _status;
+
+  Student(String firstName, String lastName, int grade){
+    this.firstName = firstName;
+    this.lastName = lastName;
+    this.grade = grade;
+  }
+
+  String get getFirstname{
+    return "OGR - " + this.firstName;
+  }
+
+  void set setFirstName(String value){
+
+    this.firstName = value;
+  }
+
+  String get getStatus{
+    String message = "";
+    if (this.grade >= 50) {
+      message = "Geçti";
+    } else if (this.grade >= 40) {
+      message = "Bütünlemeye kaldı";
+    } else {
+      message = "Kaldı";
+    }
+    return message;
+  }
+}
